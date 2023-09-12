4514180506c6cabfffa158a020ab08208c524dee
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-09-12 20:21:56 +0800

    ⭐️ Impl: Ex - Add Example Project

---
+++ example/BlurViewExample/BlurTestViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BlurTestViewController.swift
+//  BlurViewExample
+//
+//  Created by Dominic Go on 9/12/23.
+//
+
+import UIKit
+
+class BlurTestViewController: UIViewController {
+  
+  override func loadView() {
+    let view = UIView();
+    self.view = view;
+    
+    view.backgroundColor = .white;
+  };
+};
