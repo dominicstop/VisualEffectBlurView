65339f54bd9bd00402b932b08f45ac0f84629ba6
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-09-14 19:05:39 +0800

    ⭐️ Impl: Add Support for SwiftUI

---
+++ VisualEffectBlurView/Sources/VisualEffectBlurView/VisualEffectBlur.swift
@@ -0,0 +1,39 @@
+//
+//  VisualEffectBlur.swift
+//  
+//
+//  Created by Dominic Go on 9/14/23.
+//
+
+import SwiftUI
+
+
+@available(iOS 13.0, *)
+public struct VisualEffectBlur: UIViewRepresentable {
+
+  @Binding public var blurEffectStyle: UIBlurEffect.Style;
+  @Binding public var blurRadius: Double?;
+  
+  public init(
+    blurEffectStyle: Binding<UIBlurEffect.Style>,
+    blurRadius: Binding<Double?> = .constant(nil)
+  ) {
+    self._blurEffectStyle = blurEffectStyle;
+    self._blurRadius = blurRadius;
+  };
+
+  public func makeUIView(context: Context) -> VisualEffectBlurView {
+    VisualEffectBlurView(blurEffectStyle: self.blurEffectStyle);
+  };
+
+  public func updateUIView(
+    _ view: VisualEffectBlurView,
+    context: Context
+  ) {
+    view.blurEffectStyle = self.blurEffectStyle;
+    
+    if let blurRadius = self.blurRadius {
+      view.blurRadius = CGFloat(blurRadius);
+    };
+  };
+};
