d137cb9ae91a4f9bfa297364bb42b9e20cd30ea0
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-09-14 21:29:41 +0800

    🐞 Fix: SPM Package Config

--- Package.swift
+++ Package.swift
@@ -24,9 +24,10 @@ let package = Package(
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "VisualEffectBlurView",
-      path: "Sources",
+      path: "VisualEffectBlurView",
       linkerSettings: [
 				.linkedFramework("UIKit"),
+        .linkedFramework("SwiftUI")
 			]
     ),
   ]
