e6318e8333309d9ff4ea4c8ecf0686e060450bfd
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-09-16 07:19:45 +0800

    ⭐️ Impl: `UIBlurEffect.Style` description Property

---
+++ VisualEffectBlurView/Sources/Extensions/UIBlurEffect.Style+CustomStringConvertible.swift
@@ -0,0 +1,79 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dominic Go on 9/16/23.
+//
+
+import UIKit
+
+
+extension UIBlurEffect.Style: CustomStringConvertible {
+  
+  public var description: String {
+    switch self {
+      case .extraLight:
+        return "extraLight";
+        
+      case .light:
+        return "light";
+        
+      case .dark:
+        return "dark";
+        
+      case .regular:
+        return "regular";
+        
+      case .prominent:
+        return "prominent";
+        
+      case .systemUltraThinMaterial:
+        return "systemUltraThinMaterial";
+        
+      case .systemThinMaterial:
+        return "systemThinMaterial";
+        
+      case .systemMaterial:
+        return "systemMaterial";
+        
+      case .systemThickMaterial:
+        return "systemThickMaterial";
+        
+      case .systemChromeMaterial:
+        return "systemChromeMaterial";
+        
+      case .systemUltraThinMaterialLight:
+        return "systemUltraThinMaterialLight";
+        
+      case .systemThinMaterialLight:
+        return "systemThinMaterialLight";
+        
+      case .systemMaterialLight:
+        return "systemMaterialLight";
+        
+      case .systemThickMaterialLight:
+        return "systemThickMaterialLight";
+        
+      case .systemChromeMaterialLight:
+        return "systemChromeMaterialLight";
+        
+      case .systemUltraThinMaterialDark:
+        return "systemUltraThinMaterialDark";
+        
+      case .systemThinMaterialDark:
+        return "systemThinMaterialDark";
+        
+      case .systemMaterialDark:
+        return "systemMaterialDark";
+        
+      case .systemThickMaterialDark:
+        return "systemThickMaterialDark";
+        
+      case .systemChromeMaterialDark:
+        return "systemChromeMaterialDark";
+        
+      @unknown default:
+        return "@unknown";
+    };
+  };
+};
