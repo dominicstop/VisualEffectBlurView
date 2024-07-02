6a1789269d2ca791d02923988064bab535b0e1d0
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-07-03 05:01:47 +0800

    📦 Package: Raise Min. Version for `DGSwiftUtilities`

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/dominicstop/DGSwiftUtilities",
-      .upToNextMajor(from: "0.23.0")
+      .upToNextMajor(from: "0.24.0")
     ),
   ],
   targets: [
--- VisualEffectBlurView.podspec
+++ VisualEffectBlurView.podspec
@@ -23,5 +23,5 @@ Pod::Spec.new do |s|
   s.source_files = 'Sources/**/*'
   s.frameworks = 'UIKit'
   
-  s.dependency 'DGSwiftUtilities', '~> 0.23'
+  s.dependency 'DGSwiftUtilities', '~> 0.24'
 end
