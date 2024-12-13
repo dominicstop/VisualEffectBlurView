1e603aef38184adc2112c1120a3bf71f293139d7
Dominic Go <dominic@dominicgo.dev>
2024-12-13 18:58:27 +0800

    ⭐️ Impl: `VisualEffectCustomFilterView`

---
+++ Sources/Views/VisualEffectCustomFilterView.swift
@@ -0,0 +1,28 @@
+//
+//  VisualEffectCustomFilterView.swift
+//
+//
+//  Created by Dominic Go on 12/13/24.
+//
+
+import UIKit
+import DGSwiftUtilities
+
+@available(iOS 13, *)
+public class VisualEffectCustomFilterView: VisualEffectView {
+  
+  public convenience init(
+    withInitialFilters initialFilters: [LayerFilterType]
+  ) throws {
+    let dummyEffect = UIBlurEffect(style: .regular);
+    try self.init(withEffect: dummyEffect);
+    try self.immediatelyApplyFilters(initialFilters);
+  };
+  
+  public func immediatelyApplyFilters(_ nextFilters: [LayerFilterType]) throws {
+    try self.setFiltersViaEffectDesc(
+      withFilterTypes: nextFilters,
+      shouldImmediatelyApplyFilter: true
+    );
+  };
+};
