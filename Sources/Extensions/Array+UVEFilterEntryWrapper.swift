a7f8117f01ac44af0de6af5aebeaa6cc07377dd5
Dominic Go <dominic@dominicgo.dev>
2024-12-31 15:37:23 +0800

    🛠 Refactor: Move Helper to `Array+UVEFilterEntryWrapper`

--- Sources/Extensions/Array+UVEFilterEntryWrapper.swift
+++ Sources/Extensions/Array+UVEFilterEntryWrapper.swift
@@ -9,6 +9,78 @@ import Foundation
 
 
 public extension Array where Element == UVEFilterEntryWrapper {
+
+  typealias FilterEntryTypePair = (
+    filterType: LayerFilterType,
+    filterTypeIndex: Int,
+    filterEntryWrapped: Element
+  );
+  
+  func paired(
+    withFilterTypes filterTypes: [LayerFilterType],
+    shouldUseReferenceEqualityForImageComparison: Bool = false
+  ) -> (
+    orphanedFilterEntries: Self,
+    orphanedFilterTypes: [LayerFilterType],
+    matches: [FilterEntryTypePair]
+  ) {
+    var orphanedFilterEntries: Self = [];
+    
+    let matches: [FilterEntryTypePair] = self.compactMap { filterEntryWrapped in
+      let match = filterTypes.enumerated().first {
+        guard $0.element.decodedFilterName == filterEntryWrapped.filterKind else {
+          return false;
+        };
+        
+        if !shouldUseReferenceEqualityForImageComparison {
+          return true;
+        };
+        
+        let filterEntryAsLayerType =
+          LayerFilterType(fromWrapper: filterEntryWrapped);
+      
+        switch ($0.element, filterEntryAsLayerType) {
+          case (
+            let .variadicBlur(_, maskImageNext, _, _, _),
+            let .variadicBlur(_, maskImageCurrent, _, _, _)
+          ):
+            return maskImageNext === maskImageCurrent;
+            
+            default:
+              return true;
+        };
+      };
+      
+      guard let match = match else {
+        orphanedFilterEntries.append(filterEntryWrapped);
+        return nil;
+      };
+      
+      return (
+        filterType: match.element,
+        filterTypeIndex: match.offset,
+        filterEntryWrapped: filterEntryWrapped
+      );
+    };
+    
+    let orphanedFilterTypes = filterTypes.filter { filter in
+      let match = matches.first {
+        filter.decodedFilterName == $0.filterType.decodedFilterName;
+      };
+      
+      return match == nil;
+    };
+    
+    return (orphanedFilterEntries, orphanedFilterTypes, matches);
+  };
+  
+  func onlyElementsWithMatches(
+    withFilterTypes filterTypes: [LayerFilterType]
+  ) -> Self {
+    self.paired(withFilterTypes: filterTypes).matches.map {
+      $0.filterEntryWrapped;
+    };
+  };
   
   func updateFilterValuesRequested(with newFilter: LayerFilterType) throws {
     let filterMatch = self.first {
