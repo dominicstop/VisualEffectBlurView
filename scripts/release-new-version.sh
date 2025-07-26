c61a7ce5c0c58c46ae0bc415ebcc458885da64fc
Dominic Go <dominic@dominicgo.dev>
2025-07-26 18:02:36 +0800

    ⚙️ Chore: Add Scripts

---
+++ scripts/release-new-version.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+# NOTE:
+# assumes the script is run on repo root
+
+SCRIPT_BASE_URL="https://raw.githubusercontent.com/dominicstop/DGSwiftUtilities/refs/heads/main/scripts"
+
+# E.g. delete-version.sh
+SCRIPT_NAME=$(basename "$0")
+
+SCRIPT_URL="${SCRIPT_BASE_URL}/${SCRIPT_NAME}"
+TEMP_SCRIPT_NAME="temp-$SCRIPT_NAME"
+TEMP_SCRIPT_PATH="/tmp/$TEMP_SCRIPT_NAME"
+
+echo "Script Repo URL: $SCRIPT_BASE_URL"
+echo "Script: $SCRIPT_NAME"
+
+echo "Fetching script..."
+curl -s $SCRIPT_URL -o $TEMP_SCRIPT_PATH
+chmod +x $TEMP_SCRIPT_PATH
+
+echo "Executing script: $TEMP_SCRIPT_PATH"
+$TEMP_SCRIPT_PATH ; 
+
+echo "Cleaning up..."
+rm -f $TEMP_SCRIPT_PATH
